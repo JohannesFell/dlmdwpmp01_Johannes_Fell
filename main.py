bbdcc45f940eb5c68a618dbdaf71057c693b550f
johannesfell <johannes.fell@magenta.de>
2023-03-19 11:52:26 +0100

    create model directory and adding functions to create, connect,read and write to database.

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-# This is a sample Python script.
+import model.database as database
 
-# Press Umschalt+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # DB Verbindung herstellen
+    __connectionStr__ = 'sqlite:///dlmdwpmp01.db'
+    db_instance = database.Database(__connectionStr__, '')
+    conn_meta = db_instance.connectToDb()
+    metadata = conn_meta[1]
+
+    # Trainings- und Idealfunktionen in DB speichern
+    db_instance.loadDataToDatabase('Trainingsfunktionen', 'train.csv')
+    db_instance.loadDataToDatabase('Idealfunktionen', 'ideal.csv')
+
+    # Daten aus Datenbank laden
+    df_train = db_instance.get_dataframe_from_Db('Trainingsfunktionen')
+    df_ideal = db_instance.get_dataframe_from_Db('Idealfunktionen')
+
---
+++ model/database.py
@@ -0,0 +1,50 @@
+import pandas as pd
+import sqlalchemy as db
+
+
+class Database:
+
+    def __init__(self, conn_str, connection):
+        self.conn_str = conn_str
+        self.connection = connection
+
+    def connectToDb(self):
+        engine = db.create_engine(self.conn_str) 
+        print('Engine: ', engine)
+
+        # DB Verbindungsobjekt
+        connection = engine.connect()
+        self.connection = connection
+
+        return self.connection, db.MetaData()
+
+    def loadDataToDatabase(self, tablename, data):
+        """
+        Daten in die übergebene Tabelle laden
+        :param tablename: Tabellennamen
+        :param data: Dateinamen
+        :return: NONE
+        """
+
+        if not isinstance(data, pd.DataFrame):
+            # .csv-Datei in Tabelle laden
+            df = pd.read_csv(data)
+        else:
+            df = data
+
+        df.to_sql(con=self.connection.engine, name=tablename, if_exists='replace', index=False)
+        meta = db.MetaData()
+        meta.create_all(self.connection.engine)
+
+    def get_dataframe_from_Db(self, tablename):
+        """
+        Daten aus Datenbanktabelle laden
+        :param tablename: Tabellennamen
+        :return: Dataframe des Inhaltes der Tabelle
+        """
+        table_df = pd.read_sql_table(
+            tablename,
+            self.connection.engine
+        )
+        table_df.set_index('x', inplace=True)
+        return table_df
